439c40d28215d358e6146cbc8fc569c658fa539a
ncglasgo <71346534+ncglasgo@users.noreply.github.com>
2020-09-16 20:55:44 -0400

    My First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var boxEle = document.body.querySelector(".box");
+
+var num1 = prompt("Enter the First Number");
+var num2 = prompt("Enter the Second Number");
+
+var sum = parseInt(num1) + parseInt(num2);
+
+document.write('The Sum of ' + num1 + ' + ' + num2 + ' = ' + sum);
